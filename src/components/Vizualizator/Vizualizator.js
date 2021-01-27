4c0936b5139a45a9ad3d01fe4a2ba15471f6ee8b
Mikhail Zakharov <zakharov3010@yandex.ru>
2021-01-27 15:24:30 +0300

    fix: format date for display users

--- src/components/Vizualizator/Vizualizator.js
+++ src/components/Vizualizator/Vizualizator.js
@@ -3,12 +3,16 @@ import './Vizualizator.css';
 
 function Vizualizator({ info, date, civilTwilightBeginInPercent, civilTwilightEndInPercent }) {
 
+    function formatDate(date) {
+        return date.split('-').reverse().join().replace(/,/g,'/');
+    }
+
     return (
         <div className={'main'}>
-            <h1 className={'main__title'}>{date}</h1>
-            <p>sunrise: {info.sunrise}</p>
-            <p>sunset: {info.sunset}</p>
-            <p>length: {info.day_length}</p>
+            <h1 className={'main__title'}>{formatDate(date)}</h1>
+            <p>Sunrise: {info.sunrise}</p>
+            <p>Sunset: {info.sunset}</p>
+            <p>Length: {info.day_length}</p>
             <div className={'main__diagramma'}
                  style={{background: `linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(0,212,255,1) ${civilTwilightBeginInPercent}%, rgba(0,212,240,1) ${civilTwilightEndInPercent}%, rgba(2,0,36,1) 100%)`}}/>
             <Link to='/' className={'main__link'}>Начать</Link>
